3fa59634a7d0482be7a14ef7673ce89e5bf24a31
PremaDongare <premadongare17@gmail.com>
2026-01-28 09:25:27 +0530

    Assignment 5

---
+++ Assignment5/Assignment5.py
@@ -0,0 +1,41 @@
+from nltk.corpus import wordnet as wn
+
+
+def get_semantic_relations(word):
+    synonyms = set()
+    antonyms = set()
+    hypernyms = set()
+
+    # Get synsets of the word
+    for synset in wn.synsets(word):
+        
+        # Synonyms
+        for lemma in synset.lemmas():
+            synonyms.add(lemma.name())
+
+            # Antonyms
+            if lemma.antonyms():
+                antonyms.add(lemma.antonyms()[0].name())
+
+        # Hypernyms 
+        for hyper in synset.hypernyms():
+            hypernyms.add(hyper.name())
+
+    return synonyms, antonyms, hypernyms
+
+
+# -------- MAIN PROGRAM --------
+word = input("Enter a word: ")
+
+synonyms, antonyms, hypernyms = get_semantic_relations(word)
+
+print("\nWord:", word)
+
+print("\nSynonyms:")
+print(synonyms if synonyms else "No synonyms found")
+
+print("\nAntonyms:")
+print(antonyms if antonyms else "No antonyms found")
+
+print("\nHypernyms:")
+print(hypernyms if hypernyms else "No hypernyms found")
